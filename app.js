afff42a63fe87858d2b490a3cb6c6212e123f0dd
Your Name <[haywee39@gmail.com]>
2025-04-11 19:50:58 +0100

    home page redesign

--- app.js
+++ app.js
@@ -1,81 +1,51 @@
-// Typewriter Effect
-const text1 = "THE PETERSTOUCH MERCHANTS";
-const text2 = "Your trusted partner for state-of-the-art LED Displays, cutting-edge audio-visual solutions, impactful publicity equipments.";
 
-const typingSpeed = 75;
-let i = 0;
-let j = 0;
-
-function typeWriter1() {
-  const textElement1 = document.getElementById("text");
-  if (i < text1.length) {
-    textElement1.textContent += text1.charAt(i);
-    i++;
-    setTimeout(typeWriter1, typingSpeed);
-  } else {
-    setTimeout(typeWriter2, 450); // Delay before starting the second typewriter
-  }
-}
-
-function typeWriter2() {
-  const textElement2 = document.getElementById("desc");
-  if (j < text2.length) {
-    textElement2.textContent += text2.charAt(j);
-    j++;
-    setTimeout(typeWriter2, typingSpeed);
-  }
-}
-
-// Background Image Rotator
-const backgroundImages = [
-  "./backgrdimg/client2.jpg",
-  "./backgrdimg/backgrd.jpg",
-  "./backgrdimg/ptrninstlr.jpg",
-  "./backgrdimg/conf-seat.jpg",
-  "./backgrdimg/scrnguyyelow.jpg",
-  "./backgrdimg/mic-conf.jpg"
-  
+const homeSection = document.getElementById('home');
+const images= [
+  'url("./backgrdimg/backgrd.jpg")',
+  'url("./backgrdimg/mixr.jpg")',
+  'url("./backgrdimg/mic-conf.jpg")'
+  // "",
+  // "./backgrdimg/mic-conf.jpg"
 ];
+let current = 0;
 
-let currentImageIndex = 0;
-
-function rotateBackground() {
-  const homeSection = document.getElementById("home");
-  homeSection.style.backgroundImage = `url('${backgroundImages[currentImageIndex]}')`;
-  currentImageIndex = (currentImageIndex + 1) % backgroundImages.length; // Loop through images
+function changeBackground() {
+  homeSection.style.backgroundImage = images[current];
+  current = (current + 1) % images.length;
 }
 
-window.onload = () => {
-  typeWriter1();
-  rotateBackground();
-  setInterval(rotateBackground, 3000); // Change background every 5 seconds
-};
 
-// ************************************************************************
+changeBackground(); 
+setInterval(changeBackground, 3000);
 
+// window.onload = () => {
+//   typeWriter1();
+//   rotateBackground();
+//   setInterval(rotateBackground, 5000); // Change background every 5 seconds
+// };
 // ***************************************************************
 
 // COMPANY LOGO APPEARS ON Slide in TO THE POINT 
 
-document.addEventListener("DOMContentLoaded", function () {
-  const image = document.querySelector(".slide-in-image");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const image = document.querySelector(".slide-in-image");
 
-  function handleScroll() {
-      const imageRect = image.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
+//   function handleScroll() {
+//       const imageRect = image.getBoundingClientRect();
+//       const windowHeight = window.innerHeight;
 
-      // Check if the image is in the viewport
-      if (imageRect.top < windowHeight && imageRect.bottom > 0) {
-          image.classList.add("visible");
-      } 
-      // Remove 'visible' class only when the image is completely out of view
-      else if (imageRect.bottom <= 0 || imageRect.top >= windowHeight) {
-          image.classList.remove("visible");
-      }
-  }
+//       // Check if the image is in the viewport
+//       if (imageRect.top < windowHeight && imageRect.bottom > 0) {
+//           image.classList.add("visible");
+//       } 
+//       // Remove 'visible' class only when the image is completely out of view
+//       else if (imageRect.bottom <= 0 || imageRect.top >= windowHeight) {
+//           image.classList.remove("visible");
+//       }
+//   }
 
-  window.addEventListener("scroll", handleScroll);
-});
+//   window.addEventListener("scroll", handleScroll);
+// });
 
 // *********************************************************************************
 // Select all boxes
